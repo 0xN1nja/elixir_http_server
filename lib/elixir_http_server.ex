b9096c7b40ee48b9853a90cd9721fcb74a59d546
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2023-02-16 23:22:17 +0530

    Added HTTP Server

--- lib/elixir_http_server.ex
+++ lib/elixir_http_server.ex
@@ -1,18 +1,36 @@
 defmodule ElixirHttpServer do
-  @moduledoc """
-  Documentation for `ElixirHttpServer`.
-  """
 
-  @doc """
-  Hello world.
+  def main do
+    {:ok, port} =
+      case System.argv do
+       [port_string] -> {:ok, String.to_integer(port_string)}
+       not_a_port -> {:error, {:invalid_port, not_a_port}}
+      end
+    start(port)
+  end
 
-  ## Examples
+  def start(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, {:active, true}, {:reuseaddr, true}])
+    IO.puts("Server Listening On http://localhost:#{port}/")
+    _accept(socket)
+  end
 
-      iex> ElixirHttpServer.hello()
-      :world
+  defp _accept(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    spawn(fn ->
+      handle_connection(client)
+    end)
+    _accept(socket)
+  end
 
-  """
-  def hello do
-    :world
+  defp handle_connection(client) do
+    send_response(client, "HTTP 1.1 200 OK\r\n\r\n\r\n<!DOCTYPE html><head><title>Elixir HTTP Server</title></head><body><h1>Hello, Elixir!</h1><br><a href='https://github.com/0xN1nja' target='_blank'>Abhimanyu Sharma</a></body></html>")
+    :ok = :gen_tcp.close(client)
+  end
+
+  defp send_response(client, msg) do
+    :gen_tcp.send(client, msg)
   end
 end
+
+ElixirHttpServer.main()
